cf10277252fb921a47f7824e924862f0ab43cd6c
马飞飞 <ffma@Ctrip.com>
2015-02-13 09:03:52 +0800

    a hello world demo use SOA2.0

---
+++ ConsoleHelloWorldService/ConsoleHelloWorldService/HelloWorldServiceImpl.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CServiceStack.Common.Types;
+using CServiceStack.ServiceHost;
+
+namespace ConsoleHelloWorldService
+{
+    public class HelloWorldServiceImpl : IHelloWorldService
+    {
+        [Route("/sayHello")]
+        public HelloResponseType Hello(HelloRequestType helloWorldIn)
+        {
+            return new HelloResponseType { Result = "Hello " + helloWorldIn.Name };
+        }
+
+        [Route("/check")]
+        public CheckHealthResponseType CheckHealth(CheckHealthRequestType healthCheckIn)
+        {
+            return new CheckHealthResponseType();
+        }
+    }
+}
---
+++ ConsoleHelloWorldService/testLib/HelloRequestType.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testLib
+{
+    public class HelloRequestType
+    {
+        private string nameField;
+
+        /// <summary>
+        /// If provided, service will response with Hello 'Name'.
+        /// </summary>
+        //[ProtoMember(1)]
+        public string Name
+        {
+            get
+            {
+                return this.nameField;
+            }
+            set
+            {
+                this.nameField = value;
+            }
+        }
+    }
+}
